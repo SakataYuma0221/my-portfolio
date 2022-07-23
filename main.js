651de8e816f5228aee35eac2252a4e63c4dee41c
koutarou0311 <s.yuu-gin.1311@ezweb.ne.jp>
2022-07-23 18:01:51 +0900

    ポートフォリオサイトの開発を進めました

---
+++ main.js
@@ -0,0 +1,58 @@
+const quizText = document.getElementById("quiz-text")
+const choice1 = document.getElementById("choice-1")
+const choice2 = document.getElementById("choice-2")
+const choice3 = document.getElementById("choice-3")
+const feedback = document.getElementById("feedback")
+
+// クイズの内容
+const quiz = {
+  text: "僕の好きな食べ物はなんでしょう？",
+  choices: [
+    {
+      text: "もも",
+      feedback:
+        "残念！",
+    },
+    {
+      text: "梨",
+      feedback: "正解！",
+    },
+    {
+      text: "りんご",
+      feedback: "残念！",
+    },
+  ],
+}
+
+// quiz を画面に表示する関数
+const reloadQuiz = function () {
+  // 問題文を表示
+  quizText.textContent = "Q. " + quiz.text
+
+  // 選択肢（ボタン）の中身を表示
+  choice1.textContent = quiz.choices[0].text
+  choice2.textContent = quiz.choices[1].text
+  choice3.textContent = quiz.choices[2].text
+}
+
+// choiceNumber番目の選択肢を選択
+const choose = function (choiceNumber) {
+  // フィードバックを表示
+  feedback.textContent = quiz.choices[choiceNumber].feedback
+}
+
+choice1.onclick = function () {
+  // 0 番目の選択肢を選択
+  choose(0)
+}
+choice2.onclick = function () {
+  // 1 番目の選択肢を選択
+  choose(1)
+}
+choice3.onclick = function () {
+  // 2 番目の選択肢を選択
+  choose(2)
+}
+
+// reloadQuiz関数 を実行して、クイズを画面に表示する
+reloadQuiz()
